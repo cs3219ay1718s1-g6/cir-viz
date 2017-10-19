1f8064f490c2cc444ee27821baf35a190b37dd92
Mai Anh Vu <me@maianhvu.com>
2017-10-19 21:22:08 +0800

    add top authors

--- main.js
+++ main.js
@@ -9,25 +9,29 @@ const neo4jPass = process.env.NEO4J_PASSWORD
 const app = express()
 const driver = neo4j.driver(databaseUri, neo4j.auth.basic(neo4jUser, neo4jPass))
 const session = driver.session()
+const { getVenueId } = require('./processing/utils')
 
-session.run(
-    'CREATE (a:Person {name: $name}) RETURN a',
-    { name: 'Alice' }
-).then(result => {
-    session.close()
-    const record = result.records[0]
-    const node = record.get(0)
-    console.log(node.properties.name)
-    driver.close()
-}).catch(error => {
-    console.error(error)
+// Routes
+app.get('/authors/top', (req, res) => {
+    let count = parseInt(req.query.count) || 10
+    let query = 'MATCH (a:Author)-[:CONTRIB_TO]->(p:Paper)'
+    if (req.query.venue) {
+        let venueId = getVenueId(req.query.venue.toString().trim())
+        query += `-[:WITHIN]->(v:Venue) WHERE v.venueID = '${venueId}'`
+    }
+    query += ' WITH a.authorName AS AuthorName, COUNT(p) AS Papers'
+    query += ' ORDER BY Papers DESC '
+    query += ` RETURN AuthorName, Papers LIMIT ${count};`
+    session.run(query).then(result => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(result.records.map(r => ({
+            author: r.get(0),
+            count: Math.max(r.get(1).low, r.get(1).high)
+        }))))
+    }).catch(err => {
+        res.send(err.message)
+    })
 })
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('CIR server listening on port ' + port))
